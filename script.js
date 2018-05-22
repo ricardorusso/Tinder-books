34a5cf97f9722cef46dd03374f12a65973d16675
Ricardo Russo <russoricardo1@gmail.com>
2018-05-22 18:47:13 +0100

    22 maio

--- script.js
+++ script.js
@@ -40,26 +40,26 @@
 
                                                            // maneira do andre
 
-$('.book button').click(function(){
+// $('.book button').click(function(){
 
-    var opinion = $(this).attr('data-opinion');
-    $current = $('.book.active');
-    $next = $current.next();
+//     var opinion = $(this).attr('data-opinion');
+//     $current = $('.book.active');
+//     $next = $current.next();
 
-    if($next.length == 0){
-        // $next = $('.book').first(); outras formas de fazer 
-        // $next = $current.next();
-        $next = $('.book:first-of-type');
+//     if($next.length == 0){
+//         // $next = $('.book').first(); outras formas de fazer 
+//         // $next = $current.next();
+//         $next = $('.book:first-of-type');
 
         
-    }
+//     }
 
 
 
-    $current.removeClass('active');
-    $next.addClass('active');
+//     $current.removeClass('active');
+//     $next.addClass('active');
 
-});
+// });
 
  // $current = $(this).addClass('active');
     // $next = $current.removeClass('active');
@@ -86,72 +86,154 @@ $('.book button').click(function(){
 //    }
 //     });
 
-var book1 = {
-    name:'Os Lusiadas',
-    img:'livro1.jpg',
-    descricao: "Os Lusíadas é uma obra de poesia épica do escritor português Luís Vaz de Camões, considerada a \"epopeia portuguesa por excelência\". Provavelmente concluída em 15512, foi publicada pela primeira vez em 1572 no período literário do classicismo, três anos após o regresso do autor do Oriente.",
-    link:{
-        text:"Wiki",
-        url: 'https://pt.wikipedia.org/wiki/Os_Lus%C3%ADadas'
-    } 
-  }
+// var book1 = {
+//     name:'Os Lusiadas',
+//     img:'livro1.jpg',
+//     descricao: "Os Lusíadas é uma obra de poesia épica do escritor português Luís Vaz de Camões, considerada a \"epopeia portuguesa por excelência\". Provavelmente concluída em 15512, foi publicada pela primeira vez em 1572 no período literário do classicismo, três anos após o regresso do autor do Oriente.",
+//     link:{
+//         text:"Wiki",
+//         url: 'https://pt.wikipedia.org/wiki/Os_Lus%C3%ADadas'
+//     } 
+//   }
 
-var book2 = {
-    name:'O Envangelho segundo Jesus Cristo',
-    img:'livro2.jpg',
-    descricao: "O Evangelho Segundo Jesus Cristo é um romance de José Saramago que, com uma visão moderna e crítica da religião, reconta a história biblica sobre a vida de Jesus Cristo.",
-    link:{
-        text:"",
-        url: 'https://pt.wikipedia.org/wiki/O_Evangelho_segundo_Jesus_Cristo'
-    }
-}
+// var book2 = {
+//     name:'O Envangelho segundo Jesus Cristo',
+//     img:'livro2.jpg',
+//     descricao: "O Evangelho Segundo Jesus Cristo é um romance de José Saramago que, com uma visão moderna e crítica da religião, reconta a história biblica sobre a vida de Jesus Cristo.",
+//     link:{
+//         text:"",
+//         url: 'https://pt.wikipedia.org/wiki/O_Evangelho_segundo_Jesus_Cristo'
+//     }
+// }
 
-var book3 = {
-    name:'O Mundo de Gelo E Fogo',
-    img:'livro3.jpg',
-    descricao: "As Crônicas de Gelo e Fogo ou As Crónicas de Gelo e Fogo (no original em inglês: A Song of Ice and Fire) é uma série de livros de alta fantasia escrita pelo romancista e roteirista norte-americano George R. R.",    
-    link:{
-        text:"link",
-        url: 'https://pt.wikipedia.org/wiki/A_Song_of_Ice_and_Fire'
-    }
-}
+// var book3 = {
+//     name:'O Mundo de Gelo E Fogo',
+//     img:'livro3.jpg',
+//     descricao: "As Crônicas de Gelo e Fogo ou As Crónicas de Gelo e Fogo (no original em inglês: A Song of Ice and Fire) é uma série de livros de alta fantasia escrita pelo romancista e roteirista norte-americano George R. R.",    
+//     link:{
+//         text:"link",
+//         url: 'https://pt.wikipedia.org/wiki/A_Song_of_Ice_and_Fire'
+//     }
+// }
 
 
 class Library{
     constructor(){
-        this.books = [book1,book2,book3];
+        this.books = [];
         this.seenBooks = [];
-        this.Load(book1);
+        this.GetBooks("saramago");
+        
     }
 
     Load(book){
-        $('.book h1').text(book.name);
+        if(this.books.length == 0){
+            $(".endpage").css("display", "block");
+            $(".book").css("display", "none");
+            var obj = this;
+            this.seenBooks.forEach(function(v,i){
+                var like = "";
+                
+                like += "<b>" + obj.seenBooks[i].title + "</b>" + " -  " + obj.seenBooks[i].opinion + "<br>";
+                // console.log(like);
+               
+                $('#stat').append(like);
+            });
+            
+        }
+        else{
+        $('.book h1').text(book.title);
         $('.book img').attr("src",book.img);
-        $('.book a').attr("href", book.link.url);
-        $('.book a').text(book.link.text);
-        $('.book p').text(book.descricao);
+        $('.book p').text(book.description);
+        $('.book a').attr("href", book.link);
+        $('.book a').text("More Info");}
+    
+
+    
         // books.links.forEach(function(v,i){
         //     $(".book a").eq(i).text(v.text);
         //     $(".book a").eq(i).attr("href",v.url);
         // })
     }
     NextBook(){
+        
         this.seenBooks.push(this.books[0]);
         this.books.shift(); //ou slice (0,1)
         this.Load(this.books[0]);
+        
+        
+    }
+    GetBooks(search){
+        var obj = this;
+        $.ajax({
+            url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+            
+        }).done(function(data){
+            //quando o pedido ajax terminar com sucesso
+            // console.log(data);
+            data.items.forEach(function(v,i){
+                var book = {
+                    title: v.volumeInfo.title,
+                    description: v.volumeInfo.description,
+                    img: v.volumeInfo.imageLinks.thumbnail,
+                    opinion: '',
+                    link: v.volumeInfo.infoLink,
+                }
+                
+                
+                // obj.data.items.book.opinion += data-Opinion;
+                obj.books.push(book);
+            });
+            
+            obj.Load(obj.books[0]);
+        });
+
     }
 }
 
-// passar o livro q acabamos de ver para o seenbooks
 
-
+// passar o livro q acabamos de ver para o seenbookss
 
 var lib = new Library();
 $('.book button').click(function(){
+    if(lib.books.length > 0)
+    lib.books[0].opinion = $(this).attr("data-Opinion");
     lib.NextBook();
-})
+});
+
+$("#r").click(function(){
+
+    lib.seenBooks.forEach(function(v,i){
+    lib.books.push(v);
+    lib.seenBooks =[]
+
+    });
+    
+    $(".book").css("display", "block");
+    $(".endpage").css("display", "none");
+    $("#stat").empty();
+});
+
+$(".starpage button").click(function(){
+    $(".book").css("display", "block");
+ $(".search").input.search()
+ 
+ });
+// lib.seenBooks.forEach(function(v,i){
+//     var like = "";
+    
+//     like += lib.seenBooks[i].title + " " + lib.seenBooks[i].opinion + " ";
+//     console.log(like);
+//     $('#stat').html("<br>", like);
+// });
 
 
+// 1) Adaptar o código ao vosso projecto
+
+// 2) Colocar a opinião dos livros após like/dislike
+
+// 3) Criar página final onde mostrará o que aconteceu e 2 botões (um para voltar ao início e o outro para iniciar uma nova pesquisa)
+
+// O último botão por agora não fará nada
 
 
 
